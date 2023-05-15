726df9f66a8a2f7f854e8749a94ff859ef6d1cc2
kevin844 <adriqqq@outlook.es>
2023-05-15 17:10:46 -0500

    x

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import Header from './components/Header';
-import Sobre from './components/Sobre';
+import Nav from './components/Nav';
+
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <div>
-          <Link to="#">Home</Link>
-          <Link to="/sobre">Sobre mí</Link>
-          <Link to="/servicios">Servicios</Link>
-          <Link to="/proyectos">Proyectos</Link>
-          <Link to="/habilidades">Habilidades</Link>
-          <Link to="/blog">Blog</Link>
-        </div>
-        <Routes>
-          <Route path='/' Component={Sobre} />
-          <Route path='/header' Component={Header} />
-        </Routes>
+    <div className='App'>
+      <div className='Header-content'>
+        <Header />
+        <Nav />
       </div>
-    </Router>
+    </div>
+
   );
 }
 
--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -1,20 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './Header';
+
+
 function Nav() {
     return (
         <Router>
             <div>
-                <div>
+                <div className='nav-links'>
                     <Link to="#">Home</Link>
                     <Link to="/sobre">Sobre mí</Link>
                     <Link to="/servicios">Servicios</Link>
                     <Link to="/proyectos">Proyectos</Link>
                     <Link to="/habilidades">Habilidades</Link>
                     <Link to="/blog">Blog</Link>
+                    <Link to="/Contacto">Contactos</Link>
                 </div>
                 <Routes>
-                    <Route path='/' Component={Home} />
-                    <Route path='/header' Component={Header} />
+                    <Route path='#' />
+                    <Route path='/sobre' Component={Header} />
                 </Routes>
             </div>
         </Router>
     );
 }
+export default Nav;

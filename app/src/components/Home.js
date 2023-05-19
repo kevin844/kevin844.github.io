b85bbe54c8b3ebe13d93bd1a853fa180bbbdbe3e
kevin844 <adriqqq@outlook.es>
2023-05-19 15:55:15 -0500

    minimum changes

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <div className="home-content" id="home">
             <h1>Hi, my name is Kevin Gutierrez</h1>
             <h2><strong>I'm a software developer.</strong></h2>
+            <h4>Making the web a better place, one line of code at a time.</h4>
+            <button className="send-atsapp">
+                <strong>Send me a message!</strong>
+            </button>
         </div>
     );
 }

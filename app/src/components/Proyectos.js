5a90d19847233637e7a851959ff39c7f41c874c2
kevin844 <adriqqq@outlook.es>
2023-05-19 16:58:12 -0500

    skills comment

---
+++ app/src/components/Proyectos.js
@@ -0,0 +1,15 @@
+/*
+skills:
+languaje:python, php.
+framework: flask, django, laravel.
+databases: mysql, postgresql.
+front-end: html, css, javascript, bootstrap.
+front-end frameworks: react, react native
+version control: git, github.
+Testing: pytest, unittest PHPUnit, Behat.
+Integration (CI/CD): Jenkins docker, aws.
+Infrastructure as code: Ansible -chef-.
+DevSecOps: Owasp ZAP, BurpSuite, nmap...
+
+Agile, scrum.
+*/

5fbc10b550b3086a42067ec843fa772df88823cf
kevin844 <adriqqq@outlook.es>
2023-05-19 16:11:18 -0500

    styless added

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom";
+import { 
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link 
+} from "react-router-dom";
 
 function Home() {
     return (
         <div className="home-content" id="home">
             <h1>Hi, my name is Kevin Gutierrez</h1>
             <h2><strong>I'm a software developer.</strong></h2>
-            <h4>Making the web a better place, one line of code at a time.</h4>
-            <button className="send-atsapp">
-                <strong>Send me a message!</strong>
-            </button>
+            <p>Making the web a better place, one line of code at a time.</p>
+            
+            <a href="https://wa.me/+573108442794" className="wa-btn" target="_blank">Contact me</a>
         </div>
     );
 }

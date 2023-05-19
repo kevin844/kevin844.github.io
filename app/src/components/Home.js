d45eb70d2073fe12344f3e410b29c5875c32d7f2
kevin844 <adriqqq@outlook.es>
2023-05-19 16:12:59 -0500

    rel

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,10 +1,3 @@
-import { 
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link 
-} from "react-router-dom";
-
 function Home() {
     return (
         <div className="home-content" id="home">
@@ -12,7 +5,7 @@ function Home() {
             <h2><strong>I'm a software developer.</strong></h2>
             <p>Making the web a better place, one line of code at a time.</p>
             
-            <a href="https://wa.me/+573108442794" className="wa-btn" target="_blank">Contact me</a>
+            <a href="https://wa.me/+573108442794" rel="noreferrer" className="wa-btn" target="_blank">Contact me</a>
         </div>
     );
 }

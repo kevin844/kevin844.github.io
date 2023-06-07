116eac45b47fb8835cd65d3b197f3083d12a84bc
kevin844 <adriqqq@outlook.es>
2023-06-07 09:14:12 -0500

    footer and buttons

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import Nav from './components/Nav';
+import React, { useState } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Experiencia from './components/Experiencia';
@@ -8,22 +7,40 @@ import Contacto from './components/Contacto';
 import Footer from './components/Footer';
 
 import './App.css';
-import Header from './components/Header';
+
 function App() {
+
+  const [show, setShow] = useState("home");
+
+  const handleShow = (component) => {
+    setShow(component);
+  }
+
   return (
     <div className='App'>
       <div className='Header-content'>
-        <Header />
-        <Nav />
+        <div className="header">
+          <strong className="logo-letters">KG</strong>
+        </div>
+        <div className='nav-links'>
+          <button className='btn' onClick={() => handleShow("home")}>Home</button>
+          <button className='btn' onClick={() => handleShow("sobre")}>Sobre</button>
+          <button className='btn' onClick={() => handleShow("servicios")}>servicios</button>
+          <button className='btn' onClick={() => handleShow("proyectos")}>proyectos</button>
+          <button className='btn' onClick={() => handleShow("habilidades")}>habilidades</button>
+          <button className='btn' onClick={() => handleShow("blog")}>blog</button>
+          <button className='btn' onClick={() => handleShow("contacto")}>contacto</button>
+        </div>
       </div>
       <div className='body'>
-        <Home />
-        <About />
-        <Experiencia />
-        <Proyectos />
-        <Contacto />
-        <Footer />
+        {show === "home" && <Home />}
+        {show === "sobre" && <About />}
+        {show === "servicios" && <Experiencia />}
+        {show === "proyectos" && <Proyectos />}
+        {show === "habilidades" && <Proyectos />}
+        {show === "contacto" && <Contacto />}
       </div>
+      <Footer />
     </div>
   );
 }
--- app/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-
-function Header() {
-    return (
-        <div className="header">
-            <strong className="logo-letters">KG</strong>
-        </div>
-    );
-}
-export default Header;

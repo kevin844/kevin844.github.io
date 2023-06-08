9a2219c6acf1ba35422333b9ab71d8e980c1e0a0
kevin844 <adriqqq@outlook.es>
2023-06-08 09:03:37 -0500

    exp content

--- app/src/components/Experiencia.js
+++ app/src/components/Experiencia.js
@@ -2,8 +2,17 @@
 
 function Experiencia() {
     return (
-        <div>
-            <h1>Experiencia</h1>
+        <div className="exp">
+            <h2>Experience:</h2>
+            <div className="experience-content">
+                <img src="https://picsum.photos/200" alt="imagen" />
+                <div className="experience-description">
+                    <strong className="date">22 Nov 2022 - 30 Jun 2023</strong>
+                    <h3>Empresa</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                </div>
+
+            </div>
         </div>
     )
 }
--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,11 +1,12 @@
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <footer>
             <h3>Footer</h3>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
+            <a href="https://www.pexels.com">Photos provided by Pexels</a>
         </footer>
     )
 }

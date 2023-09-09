80b0c866abe22fad3e05a04fc6c4f9870095a0c7
kevin844 <adriqqq@outlook.es>
2023-09-09 10:06:00 -0500

    manifest and about section modified

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -68,30 +68,50 @@ function About() {
     ];
 
     return (
-        <div className="skillsColumn">
-            <div className="colSkill">
-                {skills.slice(0, Math.ceil(skills.length / 2)).map((skill, index) => (
-                    <div key={index}>
-                        <h3>{skill.category}</h3>
-                        <ul>
-                            {skill.list.map((item, i) => (
-                                <li key={i}>{item}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
+        <div>
+
+            <div id="about" class="about-content">
+                <h2>About Me</h2>
+                <p>
+                    Hello! I am a fullstack developer passionate about technology and creating innovative solutions.
+                    With experience in developing web and mobile applications, I have worked on challenging and exciting projects that have allowed me to improve my skills and knowledge.
+                    <br />
+                    <br />My main focus is building scalable and efficient applications using the latest technologies and tools.
+                    I love working as a team and collaborating with other developers to achieve the best results.
+                    In addition, I like to learn new things and keep up to date with technological trends and advances
+                    .<br />
+                    <br />I am always looking for new challenges and opportunities to apply my skills and knowledge.
+                    If you are looking for a reliable and committed fullstack developer, feel free to contact me.
+                    I am sure that we can work together to create innovative and successful solutions
+                    .</p>
+                <hr />
             </div>
-            <div className="colSkill">
-                {skills.slice(Math.ceil(skills.length / 2)).map((skill, index) => (
-                    <div key={index}>
-                        <h3>{skill.category}</h3>
-                        <ul>
-                            {skill.list.map((item, i) => (
-                                <li key={i}>{item}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
+
+            <div className="skillsColumn">
+                <div className="colSkill">
+                    {skills.slice(0, Math.ceil(skills.length / 2)).map((skill, index) => (
+                        <div key={index}>
+                            <h3>{skill.category}</h3>
+                            <ul>
+                                {skill.list.map((item, i) => (
+                                    <li key={i}>{item}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+                <div className="colSkill">
+                    {skills.slice(Math.ceil(skills.length / 2)).map((skill, index) => (
+                        <div key={index}>
+                            <h3>{skill.category}</h3>
+                            <ul>
+                                {skill.list.map((item, i) => (
+                                    <li key={i}>{item}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );

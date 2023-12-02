1593720ade19d38df43de2b7fc20e0fd6ae9fa02
sistemasSiscohuila <126344004+sistemasSiscohuila@users.noreply.github.com>
2023-12-02 14:10:53 -0500

    minimal changes

--- app/src/components/Proyectos.js
+++ app/src/components/Proyectos.js
@@ -1,23 +1,29 @@
-/*
-skills:
-languaje:python, php.
-framework: flask, django, laravel.
-databases: mysql, postgresql.
-front-end: html, css, javascript, bootstrap.
-front-end frameworks: react, react native
-version control: git, github.
-Testing: pytest, unittest PHPUnit, Behat.
-Integration (CI/CD): Jenkins docker, aws.
-Infrastructure as code: Ansible -chef-.
-DevSecOps: Owasp ZAP, BurpSuite, nmap...
+import './images/discordBot.png'
 
-Agile, scrum.
+/*
+    skills:
+    languaje:python, php.
+    framework: flask, django, laravel.
+    databases: mysql, postgresql.
+    front-end: html, css, javascript, bootstrap.
+    front-end frameworks: react, react native
+    version control: git, github.
+    Testing: pytest, unittest PHPUnit, Behat.
+    Integration (CI/CD): Jenkins docker, aws.
+    Infrastructure as code: Ansible -chef-.
+    DevSecOps: Owasp ZAP, BurpSuite, nmap...
+
+    Agile, scrum.
 */
 
 function Proyectos(){
     return(
         <div className='proyectos'>
-            <h1>Proyectos</h1>
+            <h2>Proyectos</h2>
+            <div>
+                <img src="discordBot.png" />
+                <h3>Project 1</h3>
+            </div>
         </div>
     );
 }
